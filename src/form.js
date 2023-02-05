e1f85cc74a04d9a619e67b2ab021bf0c4106737f
Shakhaowat Hossain Sazib <shsazibbhuiyan892939@gmail.com>
2023-02-06 00:29:21 +0600

    use formik

--- src/form.js
+++ src/form.js
@@ -1,31 +1,49 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
 
 const Form = () => {
-  return (
-    <>
 
-<div>
-    <div className='input_block'>
-        <label for="name">Name</label>
-        <input id='name' autoComplete='off' type="text" name="name" placeholder='Name:' />
-    </div>
-    <div className='input_block'>
-        <label for="email">Email</label>
-        <input id='email' autoComplete='off' type="email" name="email" placeholder='Email:' />
-    </div>
-    <div className='input_block'>
-        <label for="password">Password</label>
-        <input id='password' autoComplete='off' type="password" name="password" placeholder='Password:' />
-    </div>
-    <div className='input_block'>
-        <label for="comment">Comment</label>
-        <input id='comment' autoComplete='off' type="text" name="comment" placeholder='Comment:' />
-    </div>
-    <button>Send</button>
-</div>
-        
-    </>
-  )
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            password: "",
+            comment: "",
+        },
+        onSubmit: (values, { resetForm }) => {
+            console.log(values)
+            resetForm({ values: '' })
+        },
+    })
+
+    return (
+        <>
+            <form onSubmit={formik.handleSubmit}>
+
+                <div className='input_block'>
+                    <label htmlFor="name">Name</label>
+                    <input id='name' type="text" name="name" placeholder='Name:' onChange={formik.handleChange} value={formik.values.name} />
+                </div>
+
+                <div className='input_block'>
+                    <label htmlFor="email">Email</label>
+                    <input id='email' type="email" name="email" placeholder='Email:' onChange={formik.handleChange} value={formik.values.email} />
+                </div>
+
+                <div className='input_block'>
+                    <label htmlFor="password">Password</label>
+                    <input id='password' type="password" name="password" placeholder='Password:' onChange={formik.handleChange} value={formik.values.password} />
+                </div>
+
+                <div className='input_block'>
+                    <label htmlFor="comment">Comment</label>
+                    <input id='comment' type="text" name="comment" placeholder='Comment:' onChange={formik.handleChange} value={formik.values.comment} />
+                </div>
+
+                <button type='submit'>Send</button>
+            </form>
+        </>
+    )
 }
 
 export default Form;
